51d0b60be69a4fe8b43bf4d25ef2765b44f0c13f
tracer-pa-public <tracer@njuics.org>
2021-09-07 22:29:11 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:29:11 up 12 days, 11:33,  1 user,  load average: 3.28, 2.70, 2.55 > unitime > 957ab964b88804d36fae8dedef206af8a77863b0

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -8,16 +8,16 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
     uint32_t res = 0;
     res = src + dest;
     res = res & (0xFFFFFFFF >> (32 - data_size));
-    cpu.eflags.CF = result < src;
+    cpu.eflags.CF = res < src;
     
-    int even = 0;
+    /*int even = 0;
     uint32_t temp = res;
     for(int i = 0; i < 7; i++)
     {
         even += (temp & 0x00000001);
         temp >>= 1;
     }
-    cpu.eflags.PF = 1 - even % 2;
+    cpu.eflags.PF = 1 - even % 2;*/
     
     
     return res;
