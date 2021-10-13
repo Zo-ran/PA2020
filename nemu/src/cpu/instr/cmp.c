e3ea5c80f83af7bbf9a5d54cf310a3bd4bc91af6
tracer-pa-public <tracer@njuics.org>
2021-10-13 21:19:05 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:19:05 up 48 days, 10:23,  1 user,  load average: 2.59, 2.57, 2.59 > unitime 1634131145554 > cda383c0ee47d1ea51855d6d624f7e09796e1956

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -6,3 +6,16 @@ static void instr_execute_2op(){
     opeprad_read(&opr_src);;
     alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);
 }
+
+make_instr_impl_2op(cmp, r, rm, b)
+make_instr_impl_2op(cmp, r, rm, v)
+make_instr_impl_2op(cmp, rm, r, b)
+make_instr_impl_2op(cmp, rm, r, v)
+make_instr_impl_2op(cmp, i, rm, b)
+make_instr_impl_2op(cmp, i, rm, v)
+make_instr_impl_2op(cmp, i, r, b)
+make_instr_impl_2op(cmp, i, r, v)
+make_instr_impl_2op(cmp, a, o, b)
+make_instr_impl_2op(cmp, a, o, v)
+make_instr_impl_2op(cmp, o, a, b)
+make_instr_impl_2op(cmp, o, a, v)
