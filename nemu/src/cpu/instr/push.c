e61880fcf0915757df48e500b277d9be6afce826
tracer-pa-public <tracer@njuics.org>
2021-10-14 14:39:30 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:39:30 up 49 days,  3:43,  1 user,  load average: 2.42, 2.46, 2.56 > unitime 1634193570542 > 2c889df3142acf1a279511b662b470977a360f1

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,6 +2,9 @@
 
 int push_ebp(uint32_t eip, uint8_t opcode)
 {
+            printf("\e[0;31mfuck %x\e[0m\n", cpu.esp);
+    fflush(stdout);
+    assert(0);
     cpu.esp = cpu.esp - 4;
     OPERAND r;
     r.data_size = 32;
