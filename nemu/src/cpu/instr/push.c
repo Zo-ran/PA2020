161a61fd874fd8e84ce54748a414c89a60fc60b1
tracer-pa-public <tracer@njuics.org>
2021-10-14 14:46:17 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:46:17 up 49 days,  3:50,  1 user,  load average: 3.33, 2.85, 2.68 > unitime 1634193977344 > 724d7edb2d4b904760c84a8dfb648acff775ab8

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,9 +2,6 @@
 
 int push_ebp(uint32_t eip, uint8_t opcode)
 {
-            printf("\e[0;31mfuck %x\e[0m\n", cpu.esp);
-    fflush(stdout);
-    assert(0);
     cpu.esp = cpu.esp - 4;
     OPERAND r;
     r.data_size = 32;
@@ -12,8 +9,6 @@ int push_ebp(uint32_t eip, uint8_t opcode)
     r.val = cpu.ebp;
     r.addr = cpu.esp;
     operand_write(&r);
-        printf("\e[0;31mfuck %x\e[0m\n", cpu.esp);
-    fflush(stdout);
-    assert(0);
+
     return 1;
 }
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -6,9 +6,9 @@ make_instr_func(ret_near)
 {
     OPERAND MEM;
     MEM.data_size = data_size;
-    MEM.addr = cpu.ebp + 4;
+    MEM.addr = cpu.esp;
     operand_read(&MEM);
-    printf("\e[0;31mfuck %x\e[0m\n", cpu.ebp);
+    printf("\e[0;31mfuck %x\e[0m\n", MEM.val);
     fflush(stdout);
     assert(0);
 
