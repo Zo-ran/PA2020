4bfdb01c62532e60d129cf3fe85f16ac20a34ab6
tracer-pa-public <tracer@njuics.org>
2021-10-14 10:40:25 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:40:25 up 48 days, 23:44,  1 user,  load average: 2.27, 2.36, 2.44 > unitime 1634179224850 > c9db51f96e51d68277a7f0ed26560c677c37856

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,3 +2,22 @@
 /*
 Put the implementations of `call' instructions here.
 */
+make_instr_func(call_near)
+{
+    OPERAND imm, r;
+    //get IMM
+    imm.type = OPR_IMM;
+    imm.data_size = data_size;
+    imm.addr = eip + 1;
+    operand_read(&imm);
+    //push eip
+    cpu.esp = cpu.esp - 4;
+    r.data_size = 32;
+    r.type = OPR_MEM;
+    r.val = eip;
+    r.addr = cpu.esp;
+    operand_write(&r);
+    //change eip
+    cpu.eip += imm.val;
+    
+}
