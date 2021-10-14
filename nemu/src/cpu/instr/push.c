e7456d6f58044ebddeab01b078f82101d437c703
tracer-pa-public <tracer@njuics.org>
2021-10-14 16:37:38 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:37:38 up 49 days,  5:41,  1 user,  load average: 2.53, 2.71, 2.68 > unitime 1634200658192 > 34cc0745544a1f57c2b684bb29a5edc418edcb8c

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,18 +1,15 @@
 #include "cpu/instr.h"
 
 int push_ebp(uint32_t eip, uint8_t opcode)
-{        printf("\e[0;31mfuck %x\e[0m\n", cpu.esp);
-    fflush(stdout);
-    
-    cpu.esp = cpu.esp - 4;   printf("\e[0;31mfuck %x\e[0m\n", cpu.esp);
-    fflush(stdout);
+{  
+    cpu.esp = cpu.esp - 4;
     OPERAND r;
     r.data_size = 32;
     r.type = OPR_MEM;
     r.val = cpu.ebp;
     r.addr = cpu.esp;
     operand_write(&r);
-        printf("\e[0;31mfuck \e[0m\n");
+        printf("\e[0;31mfuck %x\e[0m\n", cpu.ebp);
     fflush(stdout);
     assert(0);
     return 1;
