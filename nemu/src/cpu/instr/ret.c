0a039beafa865ea8fd680ed07c5d76bae6843a87
tracer-pa-public <tracer@njuics.org>
2021-10-16 17:18:26 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:18:26 up 51 days,  6:22,  2 users,  load average: 2.16, 2.52, 2.66 > unitime 1634375906012 > a8777b218ac21746fc20f29c03d252c37cb9113

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -12,7 +12,7 @@ make_instr_func(ret_near)
     
     cpu.eip = MEM.val & (0xFFFFFFFF >> (32 - data_size));
     cpu.esp += data_size / 8;
-    printf("\e[0;31mfuck %X\e[0m\n", cpu.eip);
+    printf("\e[0;31mfuck %X\e[0m\n", cpu.esp);
     assert(0);
     return 0;
 }
