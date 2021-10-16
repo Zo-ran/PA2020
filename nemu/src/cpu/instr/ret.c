ec1ec4c330bc7edd272cf783cdf11df6aee5646b
tracer-pa-public <tracer@njuics.org>
2021-10-16 10:07:29 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:07:29 up 50 days, 23:11,  2 users,  load average: 2.21, 2.39, 2.50 > unitime 1634350049575 > ef9eec4c5b09883db0132318881473d9eac84c26

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -7,12 +7,10 @@ make_instr_func(ret_near)
     OPERAND MEM;
     MEM.data_size = data_size;
     MEM.type = OPR_MEM;
-    MEM.addr = cpu.ebp + 4;
+    MEM.addr = cpu.esp;
     operand_read(&MEM);
 
     cpu.eip = MEM.val;
-    printf("\e[0;31mfuck %x\e[0m\n", cpu.eip);
-    fflush(stdout);
-    assert(0);
+
     return 0;
 }
