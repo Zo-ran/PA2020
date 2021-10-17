2e2eba51979c49eb6f8a97893a78b321f1e60678
tracer-pa-public <tracer@njuics.org>
2021-10-17 09:52:48 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:52:48 up 51 days, 22:57,  2 users,  load average: 2.49, 2.52, 2.53 > unitime > 588cc227a274ee8728627fd1a113b8a8334fe73

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -1,4 +1,22 @@
 #include "cpu/instr.h"
+#include"cpu/alu.h"
 /*
 Put the implementations of `sub' instructions here.
 */
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_sub(opr_src.val & (0xFFFFFFFF >> 32 - opr_src.data_size), opr_dest.val & (0xFFFFFFFF >> 32 - opr_dest.data_size), opr_dest.data_size);
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(add, i, rm, v)
+make_instr_impl_2op(add, i, rm, b)
+make_instr_impl_2op(add, i, a, b)
+make_instr_impl_2op(add, i, a, v)
+make_instr_impl_2op(add, i, rm, bv)
+make_instr_impl_2op(add, r, rm, b)
+make_instr_impl_2op(add, r, rm, v)
+make_instr_impl_2op(add, rm, r, b)
+make_instr_impl_2op(add, rm, r, v)
