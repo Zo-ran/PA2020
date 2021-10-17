7b1ab8f8a055d95240f38788ec44bb9dfeb3d233
tracer-pa-public <tracer@njuics.org>
2021-10-17 10:28:41 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:28:41 up 51 days, 23:33,  2 users,  load average: 2.27, 2.46, 2.57 > unitime 1634437720973 > 9fc65939b20973ebc22a0e39f7e64307488ea35

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -15,7 +15,9 @@ make_instr_func(jmp_near)
         print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
         cpu.eip += offset;
-
+printf("\e[0;31mfuck !! %d\e[0m\n", data_size);
+    fflush(stdout);
+    assert(0);
         return 1 + data_size / 8;
 }
 
@@ -32,9 +34,7 @@ make_instr_func(jmp_short)
     int offset = sign_ext(rel.val, 8);
     cpu.eip += offset;
     
-    printf("\e[0;31mfuck !! %X\e[0m\n", cpu.eip + 2);
-    fflush(stdout);
-    assert(0);
+    
     return 2;
     
 }
