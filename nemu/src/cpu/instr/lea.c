f5bb6282bf0ed2f544576624173833cdac056a8a
tracer-pa-public <tracer@njuics.org>
2021-10-17 19:09:24 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:09:24 up 52 days,  8:13,  2 users,  load average: 2.30, 2.83, 2.76 > unitime 1634468964733 > e7e16d05177fb3b812f8df376ed7b2d8bd8ee03

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -4,13 +4,12 @@ Put the implementations of `lea' instructions here.
 */
 make_instr_func(lea)
 {
-    OPERAND r, m;
-    m.data_size = r.data_size = data_size;
+    opr_src.data_size = opr_dest.data_size = data_size;
     int len = 1;
-    len += modrm_r_rm(eip + 1, &r, &m);
-    operand_read(&r);
-    m.val = r.addr;
-    operand_write(&m);
+    len += modrm_r_rm(eip + 1, &opr_src, &opr_dest);
+    operand_read(&opr_src);
+    opr_dest.val = opr_src.addr;
+    operand_write(&opr_src);
     
     return len;
      
