0451530b5f9a521fe4c168b8b43fa38aea3ab3ed
tracer-pa-public <tracer@njuics.org>
2021-10-17 11:37:36 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:37:36 up 52 days, 41 min,  2 users,  load average: 3.15, 2.89, 2.75 > unitime 1634441856597 > e2500250303f3a88cd125bf6718dc4ba8dc113f9

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -3,9 +3,9 @@
 Put the implementations of `inc' instructions here.
 */
 static void instr_execute_1op(){
-    operand_read(&opr_src);
+    operand_read(&opr_dest);
     opr_src.val += 1;
-    operand_write(&opr_src);
+    operand_write(&opr_dest);
 }
 
 make_instr_impl_1op(inc, r, v)
