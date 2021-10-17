72566383cadca1915ef8cfd6382184efc0427e47
tracer-pa-public <tracer@njuics.org>
2021-10-17 11:32:47 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:32:47 up 52 days, 37 min,  2 users,  load average: 2.92, 2.81, 2.69 > unitime 1634441567088 > 6341db387b0b6fbe6e2a7a40add5b14e609e3cd0

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,4 +2,9 @@
 /*
 Put the implementations of `inc' instructions here.
 */
-static void 
+static void instr_execute_1op(){
+    
+}
+
+make_instr_impl_1op(inc, r, v)
+make_instr_impl_1op(inc, rm, v)
