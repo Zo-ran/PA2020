21947c591abe4465e8f82f24b365a10bff17f8f1
tracer-pa-public <tracer@njuics.org>
2021-10-17 20:05:39 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:05:39 up 52 days,  9:10,  2 users,  load average: 2.12, 2.40, 2.62 > unitime 1634472339736 > ec7bbbba8a9e0598eeb99274858c2d73e3be70f

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,11 +6,10 @@ make_instr_func(lea)
 {
     int len = 1;
     opr_src.data_size = opr_dest.data_size = data_size;
-    len += modrm_r_rm(eip + 1, &opr_dest, &opr_src);
+    len += modrm_r_rm(eip + 1, &opr_src, &opr_dest);
     operand_read(&opr_src);
-    operand_read(&opr_dest);
     opr_dest.val = opr_src.addr;
-    operand_write(&opr_dest);
+    //operand_write(&opr_dest);
     
     return len;
     
