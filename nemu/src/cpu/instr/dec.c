ae7a37850a981232626a51cbd64668a5a13c598f
tracer-pa-public <tracer@njuics.org>
2021-10-18 19:03:28 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:03:28 up 53 days,  8:07,  3 users,  load average: 3.45, 2.98, 2.85 > unitime 1634555008154 > f3edc1cfd1edd218637495b84598a47f4fa18513

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -1,4 +1,34 @@
 #include "cpu/instr.h"
+#include "cpu/alu.h"
 /*
 Put the implementations of `dec' instructions here.
 */
+make_instr_func(dec_r_v)
+{
+    OPERAND r;
+    r.data_size = data_size;
+    r.type = OPR_REG;
+    r.addr = opcode & 0x7;
+    int temp = cpu.eflags.CF;
+    
+    operand_read(&r);
+    r.val = alu_sub(1, r.val, r.data_size);
+    operand_write(&r);
+    cpu.eflags.CF = temp;
+    return 1;
+}
+
+make_instr_func(dec_rm_v)
+{
+    OPERAND rm;
+    int len = 1;
+    rm.data_size = data_size;
+    len += modrm_rm(eip + 1, &rm);
+    int temp = cpu.eflags.CF;
+    
+    operand_read(&rm);
+    rm.val = alu_sub(1, rm.val, data_size);
+    operand_write(&rm);
+    cpu.eflags.CF = temp;
+    return len;
+}
