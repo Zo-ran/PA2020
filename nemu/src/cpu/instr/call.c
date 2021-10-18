af5d02279faaf2bd62a4ed9d72e401b90781691a
tracer-pa-public <tracer@njuics.org>
2021-10-18 20:04:00 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:04:00 up 53 days,  9:08,  3 users,  load average: 2.08, 2.31, 2.35 > unitime 1634558640571 > d6bf912d7b1a8ece79b93e8649742093770644dd

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -12,6 +12,8 @@ make_instr_func(call_near)
     imm.data_size = data_size;
     imm.addr = eip + 1;
     operand_read(&imm);
+    printf("\e[0;31mimm: %X!!! \e[0m\n", imm.val);
+    fflush(stdout);
     //push eip
     cpu.esp = cpu.esp - 4;
     r.data_size = 32;
@@ -19,6 +21,7 @@ make_instr_func(call_near)
     r.val = cpu.eip + data_size / 8 + 1;
     r.addr = cpu.esp;
     operand_write(&r);
+    
     //change eip
     cpu.eip = imm.val + cpu.eip + data_size / 8 + 1;
     printf("\e[0;31mfuck %X!!! \e[0m\n", cpu.eip);
