5848761fcfd54b7f0d9fd9bf6193c3ea9efa42c2
tracer-pa-public <tracer@njuics.org>
2021-10-18 20:13:20 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:13:20 up 53 days,  9:17,  3 users,  load average: 3.45, 3.05, 2.66 > unitime 1634559199877 > 26943b303568008f825f3098e63d2bc2a2575ad

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,16 +4,13 @@ Put the implementations of `call' instructions here.
 */
 make_instr_func(call_near)
 {
-    printf("\e[0;31mfuck %X!!! \e[0m\n", cpu.eip);
-    fflush(stdout);
     OPERAND imm, r;
     //get IMM
     imm.type = OPR_IMM;
     imm.data_size = data_size;
     imm.addr = eip + 1;
     operand_read(&imm);
-    printf("\e[0;31mimm: %X!!! \e[0m\n", imm.val);
-    fflush(stdout);
+    
     //push eip
     cpu.esp = cpu.esp - 4;
     r.data_size = 32;
@@ -21,11 +18,13 @@ make_instr_func(call_near)
     r.val = cpu.eip + data_size / 8 + 1;
     r.addr = cpu.esp;
     operand_write(&r);
-    printf("\e[0;31mr: %X!!! \e[0m\n", r.val);
+    printf("\e[0;31meip before: %X \e[0m\n", cpu.eip);
+    fflush(stdout);
+    printf("\e[0;31mimm: %X \e[0m\n", imm.val);
     fflush(stdout);
     //change eip
     cpu.eip = imm.val + cpu.eip + data_size / 8 + 1;
-    printf("\e[0;31mfuck %X!!! \e[0m\n", cpu.eip);
+    printf("\e[0;31meip after: %X \e[0m\n", cpu.eip);
     fflush(stdout);
     return 0;
 }
