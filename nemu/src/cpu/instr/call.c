8c3a822d5cd75e2bec5df62566b0faac464f54dc
tracer-pa-public <tracer@njuics.org>
2021-10-18 22:09:51 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:09:51 up 53 days, 11:14,  3 users,  load average: 2.79, 2.81, 2.69 > unitime 1634566190835 > cf25da3c3518b6657fd11c2619613f8f2b10d997

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -19,15 +19,12 @@ make_instr_func(call_near)
     fflush(stdout);
     r.data_size = 32;
     r.type = OPR_MEM;
-    if(eip == 0x30108)
-        r.val = 0x12121212;
-    else  
-        r.val = cpu.eip + data_size / 8 + 1;
+    r.val = cpu.eip + data_size / 8 + 1;
     r.addr = cpu.esp;
     printf("\e[0;31meip 1: %X \e[0m\n", cpu.eip);
     fflush(stdout);
     operand_write(&r);
-    printf("\e[0;31meip 2: %X \e[0m\n", r.val);
+    printf("\e[0;31meip 2: %X \e[0m\n", cpu.eip);
     fflush(stdout);
     printf("\e[0;31mimm: %X \e[0m\n", imm.val);
     fflush(stdout);
