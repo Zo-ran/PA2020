3ae089b83ac7c1775ce22936197d98bade581d47
tracer-pa-public <tracer@njuics.org>
2021-10-19 16:11:07 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:11:07 up 54 days,  5:15,  4 users,  load average: 3.19, 2.67, 2.51 > unitime 1634631066860 > 3338c66d4affb890cd852bc3ae7fb344c12ebcec

--- testcase/src/movsx.c
+++ testcase/src/movsx.c
@@ -28,8 +28,8 @@ int main()
 	nemu_assert(A[4] == 4);
 	nemu_assert(b == 3);
 	nemu_assert(A[5] == 3);
-	HIT_GOOD_TRAP;
 	nemu_assert(C[1] == 0xffffff80);
+	HIT_GOOD_TRAP;
 	nemu_assert(A[0] == 0xffffff80);
 
 	
