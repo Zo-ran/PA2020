2f278a9176ad85f5645933d9c374a05d45a0371d
tracer-pa-public <tracer@njuics.org>
2021-10-19 16:41:01 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:41:01 up 54 days,  5:45,  4 users,  load average: 2.26, 2.50, 2.53 > unitime 1634632861359 > 69b3a043e398ffb8c14275e7be879f0ffd0534b

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -4,14 +4,13 @@
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_src.val = sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - data_size)), data_size);
-    opr_dest.val = sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    opr_src.val = sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - data_size)), opr_src.data_size);
+    opr_dest.val = sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - data_size)), opr_dest.data_size);
     alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);    
 }
 
 make_instr_impl_2op(cmp, i, rm, v)
 make_instr_impl_2op(cmp, i, rm, b)
-//make_instr_impl_2op(cmp, i, rm, bv);
 make_instr_impl_2op(cmp, i, a, v)
 make_instr_impl_2op(cmp, r, rm, b)
 make_instr_impl_2op(cmp, r, rm, v)
@@ -44,9 +43,7 @@ make_instr_func(cmp_i2rm_bv)
     operand_read(&imm);
     operand_read(&rm);
     imm.val = sign_ext(imm.val & 0xFF, 8);
-   /* printf("\e[0;31mimm: %x\e[0m\n", imm.val);
-    fflush(stdout);*/
-    
+  
     alu_sub(imm.val, rm.val, data_size);
     return len + 1;
 }
