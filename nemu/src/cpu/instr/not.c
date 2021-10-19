ca8816ff34f4857d87acdaa2804ff2d6db1a25f7
tracer-pa-public <tracer@njuics.org>
2021-10-19 10:17:05 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:17:05 up 53 days, 23:21,  4 users,  load average: 2.52, 2.59, 2.67 > unitime 1634609825795 > ff6ed5401a378226d205369e17ad715ac9942e49

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -8,7 +8,7 @@ make_instr_func(not_rm_b)
     OPERAND rm;
     rm.data_size = 8;
     len += modrm_rm(eip + 1, &rm);
-    rm.val = sign_ext(~rm.val & 0xFF, 8);
+    rm.val = ~rm.val;
     operand_write(&rm);
     return len;
 }
@@ -19,7 +19,7 @@ make_instr_func(not_rm_v)
     OPERAND rm;
     rm.data_size = data_size;
     len += modrm_rm(eip + 1, &rm);
-    rm.val = sign_ext(~rm.val & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    rm.val = ~rm.val;
     operand_write(&rm);
     return len;
 }
