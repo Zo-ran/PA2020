47a34d6f15e6e2dba0fa17c391b057dde971d11a
tracer-pa-public <tracer@njuics.org>
2021-10-19 16:56:48 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:56:48 up 54 days,  6:01,  4 users,  load average: 2.82, 2.55, 2.51 > unitime 1634633808575 > e8b41bf3205e6a16b25d9e11ece5b85361edc88f

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -15,8 +15,27 @@ make_instr_impl_2op(and, i, rm, v)
 make_instr_impl_2op(and, i, rm, b)
 make_instr_impl_2op(and, i, a, b)
 make_instr_impl_2op(and, i, a, v)
-make_instr_impl_2op(and, i, rm, bv)
+//make_instr_impl_2op(and, i, rm, bv)
 make_instr_impl_2op(and, r, rm, b)
 make_instr_impl_2op(and, r, rm, v)
 make_instr_impl_2op(and, rm, r, b)
 make_instr_impl_2op(and, rm, r, v)
+
+make_instr_func(and_i2rm_bv)
+{
+    int len = 1;
+    OPERAND imm, rm;
+    
+    rm.data_size = data_size;
+    len += modrm_rm(eip + 1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.data_size = 8;
+    imm.addr = eip + len;
+    operand_read(&imm);
+    operand_read(&rm);
+    imm.val = sign_ext(imm.val & 0xFF, 8);
+  
+    alu_and(imm.val, rm.val, data_size);
+    return len + 1;
+}
