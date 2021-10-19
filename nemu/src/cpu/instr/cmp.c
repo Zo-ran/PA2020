ae55c69c28760a76231b0e543c1c2dff08311727
tracer-pa-public <tracer@njuics.org>
2021-10-19 11:56:01 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:56:01 up 54 days,  1:00,  4 users,  load average: 2.41, 2.49, 2.57 > unitime 1634615761521 > 3545893f3f54856c6014a5d0a59021d3305cd924

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -38,7 +38,7 @@ make_instr_func(cmp_i2rm_bv)
     len += modrm_rm(eip + 1, &rm);
     
     imm.type = OPR_IMM;
-    imm.data_size = data_size;
+    imm.data_size = 8;
     imm.addr = eip + len;
     operand_read(&imm);
     sign_ext(imm.val, data_size);
