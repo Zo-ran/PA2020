89e37d1d38f25f8e44ae469d2b60d11aca04b962
tracer-pa-public <tracer@njuics.org>
2021-10-19 10:09:34 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:09:34 up 53 days, 23:13,  4 users,  load average: 2.77, 2.70, 2.73 > unitime 1634609374754 > d0d0d17c990fd7b45e0ebbe22448b080946f8862

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -2,3 +2,24 @@
 /*
 Put the implementations of `not' instructions here.
 */
+make_instr_func(not_rm_b)
+{
+    int len = 1;
+    OPERAND rm;
+    rm.data_size = 8;
+    len += modrm_rm(eip + 1, &rm);
+    rm.val = ~rm.val & 0xFF;
+    operand_write(&rm);
+    return len;
+}
+
+make_instr_func(not_rm_v)
+{
+    int len = 1;
+    OPERAND rm;
+    rm.data_size = data_size;
+    len += modrm_rm(eip + 1, &rm);
+    rm.val = ~rm.val & (0xFFFFFFFF >> (32 - data_size));
+    operand_write(&rm);
+    return len;
+}
