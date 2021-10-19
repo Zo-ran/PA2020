e31a395fa5014ddea998c44551f19ad62c8bf79f
tracer-pa-public <tracer@njuics.org>
2021-10-19 17:06:38 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:06:38 up 54 days,  6:10,  4 users,  load average: 2.67, 2.77, 2.64 > unitime 1634634397844 > ea5ef88a6085972f282315551de067b321562f

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -7,7 +7,9 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_and(opr_src.val & (0xFFFFFFFF >> (32 - opr_src.data_size)), opr_dest.val & (0xFFFFFFFF >> (32 - opr_dest.data_size)), opr_dest.data_size);
+    opr_dest.val = sign_ext(opr_dest.val, opr_dest.data_size);
+    opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
+    opr_dest.val = alu_and(opr_src.val, opr_dest.val, opr_dest.data_size);
     operand_write(&opr_dest);
 }
 
@@ -15,13 +17,13 @@ make_instr_impl_2op(and, i, rm, v)
 make_instr_impl_2op(and, i, rm, b)
 make_instr_impl_2op(and, i, a, b)
 make_instr_impl_2op(and, i, a, v)
-//make_instr_impl_2op(and, i, rm, bv)
+make_instr_impl_2op(and, i, rm, bv)
 make_instr_impl_2op(and, r, rm, b)
 make_instr_impl_2op(and, r, rm, v)
 make_instr_impl_2op(and, rm, r, b)
 make_instr_impl_2op(and, rm, r, v)
 
-make_instr_func(and_i2rm_bv)
+/*make_instr_func(and_i2rm_bv)
 {
     int len = 1;
     OPERAND imm, rm;
@@ -40,4 +42,4 @@ make_instr_func(and_i2rm_bv)
     rm.val = alu_and(imm.val, rm.val, data_size);
     operand_write(&rm);
     return len + 1;
-}
+}*/
