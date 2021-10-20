dbd62d68dfbf3459ff65a7dd33ed5c34fef37b33
tracer-pa-public <tracer@njuics.org>
2021-10-20 17:30:38 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:30:38 up 55 days,  6:34,  4 users,  load average: 2.26, 2.51, 2.62 > unitime 1634722237999 > c890deb0a7e958c36a7cc399553ade742546b04

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -47,7 +47,7 @@ make_instr_func(push_rm_v)
     m.addr = cpu.esp;
     m.type = OPR_MEM;
     m.data_size = data_size;
-    m.val = rm.val;
+    m.val = sign_ext(rm.val, rm.data_size);
     printf("\e[0;31mm.val : %x\e[0m\n", m.val);
     fflush(stdout);
     
