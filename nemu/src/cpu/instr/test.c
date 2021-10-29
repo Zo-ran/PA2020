05d83bee364e3684110ffbf0728122ea8a1d22f1
tracer-pa-public <tracer@njuics.org>
2021-10-29 20:22:44 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:22:44 up 64 days,  9:27,  4 users,  load average: 2.43, 2.43, 2.47 > unitime 1635510164076 > b1437c00eee4608b732801a1ae23ad1eabc1b0

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -3,7 +3,7 @@
 /*
 Put the implementations of `test' instructions here.
 */
-void instr_execute_2op(){
+static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
     opr_src.val = sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - opr_src.data_size)), opr_src.data_size);
