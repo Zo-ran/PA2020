1b979db5c774a2dd60d91c86b1c3043b095b5e7d
tracer-pa-public <tracer@njuics.org>
2021-11-01 21:17:26 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:17:26 up 67 days, 10:21,  4 users,  load average: 2.57, 2.78, 2.70 > unitime 1635772646379 > ac66f7adec58b1a6aa1b255310ffc70e5af7b3e

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -12,7 +12,7 @@ static void instr_execute_2op()
     opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);
     operand_write(&opr_dest);
 }
-make_instr_impl_2op(sub, i, rm, bv)
+
 make_instr_impl_2op(sub, i, rm, v)
 make_instr_impl_2op(sub, i, rm, b)
 make_instr_impl_2op(sub, i, a, b)
@@ -22,7 +22,7 @@ make_instr_impl_2op(sub, r, rm, v)
 make_instr_impl_2op(sub, rm, r, b)
 make_instr_impl_2op(sub, rm, r, v)
 
-/*make_instr_func(sub_i2rm_bv) {
+make_instr_func(sub_i2rm_bv) {
     OPERAND imm, rm;
     int len = 1;
     // read rm
@@ -38,8 +38,8 @@ make_instr_impl_2op(sub, rm, r, v)
     imm.val = sign_ext(imm.val, 8);
     
     // sub
-    rm.val = alu_sub(imm.val, rm.val, 8);
+    rm.val = alu_sub(imm.val, rm.val, data_size);
     operand_write(&rm);
     
     return len + 1;
-}*/
+}
