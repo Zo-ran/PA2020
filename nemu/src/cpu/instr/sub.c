aed2938a29c09d033ba54175de1a45c242dfbfa3
tracer-pa-public <tracer@njuics.org>
2021-11-01 17:55:41 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:55:41 up 67 days,  7:00,  4 users,  load average: 2.40, 2.71, 2.63 > unitime 1635760541028 > de440268f2014014193ee76e836e0e256c67df74

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -9,7 +9,7 @@ static void instr_execute_2op()
     operand_read(&opr_dest);
     opr_dest.val = sign_ext(opr_dest.val, opr_dest.data_size);
     opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
-    opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_src.data_size);
+    opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);
     operand_write(&opr_dest);
 }
 
@@ -17,8 +17,29 @@ make_instr_impl_2op(sub, i, rm, v)
 make_instr_impl_2op(sub, i, rm, b)
 make_instr_impl_2op(sub, i, a, b)
 make_instr_impl_2op(sub, i, a, v)
-make_instr_impl_2op(sub, i, rm, bv)
 make_instr_impl_2op(sub, r, rm, b)
 make_instr_impl_2op(sub, r, rm, v)
 make_instr_impl_2op(sub, rm, r, b)
 make_instr_impl_2op(sub, rm, r, v)
+
+make_instr_func(sub_i2rm_bv) {
+    OPERAND imm, rm;
+    int len = 1;
+    // read rm
+    rm.data_size = data_size;
+    len += modrm_rm(eip + 1, &rm);
+    operand_read(&rm);
+    
+    //read imm
+    imm.data_size = 8;
+    imm.addr = eip + len;
+    imm.type = OPR_IMM;
+    operand_read(&imm);
+    imm.val = sign_ext(imm.val, 8);
+    
+    // sub
+    rm.val = alu_sub(imm.val, rm.val, data_size);
+    operand_write(&rm);
+    
+    return len + 1;
+}
