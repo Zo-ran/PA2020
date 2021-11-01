bac4f35fadc094f175a73c34ff67e1f63c90905f
tracer-pa-public <tracer@njuics.org>
2021-11-01 21:09:15 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:09:15 up 67 days, 10:13,  4 users,  load average: 2.77, 2.78, 2.64 > unitime 1635772155332 > c4b060b03d87b5531a574fce72f0dfe354efcaca

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -38,7 +38,7 @@ make_instr_func(sub_i2rm_bv) {
     imm.val = sign_ext(imm.val, 8);
     
     // sub
-    rm.val = alu_sub(imm.val, rm.val, 8);
+    rm.val = alu_sub(imm.val, rm.val, data_size);
     operand_write(&rm);
     
     return len + 1;
