7b434665adcc468df5e31abdab01dc5959690363
tracer-pa-public <tracer@njuics.org>
2021-12-05 18:10:08 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:10:08 up 101 days,  7:14,  4 users,  load average: 3.00, 2.75, 2.61 > unitime 1638699008579 > 3e800604b59bd74deac2397828fdba0457165638

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -11,12 +11,12 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 void load_sreg(uint8_t sreg)
 {
     uint32_t offset = cpu.segReg[sreg].index + cpu.gdtr.base;
-	SegDesc *segdesc = hw_mem + offset;
+	SegDesc *segdesc = (SegDesc *)hw_mem + offset;
 	segdesc->val[0] = laddr_read(offset, 4);
 	segdesc->val[1] = laddr_read(offset + 4, 4);
-	cpu.segReg[sreg].base = (segdesc->base_15_0 & 0xffff) + ((gset->base_23_16 & 0xff) << 16) + ((get->base_31_24 & 0xff) << 24);
-	cpu.segReg[sreg].limit = (get->limit_15_0 & 0xffff) + ((get->limit_19_16 & 0xf) << 16);
-	cpu.segReg[sreg].type = (get->type & 0xf) + ((get->segment_type & 0x1) << 4);
-	cpu.segReg[sreg].privilege_level = get->privilege_level;
-	cpu.segReg[sreg].soft_use = get->soft_use & 0x1;
+	cpu.segReg[sreg].base = (segdesc->base_15_0 & 0xffff) + ((segdesc->base_23_16 & 0xff) << 16) + ((segdesc->base_31_24 & 0xff) << 24);
+	cpu.segReg[sreg].limit = (segdesc->limit_15_0 & 0xffff) + ((get->limit_19_16 & 0xf) << 16);
+	cpu.segReg[sreg].type = (segdesc->type & 0xf) + ((segdesc->segment_type & 0x1) << 4);
+	cpu.segReg[sreg].privilege_level = segdesc->privilege_level;
+	cpu.segReg[sreg].soft_use = segdesc->soft_use & 0x1;
 }
