4a865f9be0abfb75ebe3ec6006cad3a3ca64c665
tracer-pa-public <tracer@njuics.org>
2021-12-05 18:14:14 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:14:14 up 101 days,  7:18,  4 users,  load average: 3.20, 3.03, 2.76 > unitime 1638699254657 > 5b6a7c5e9d9087a3d6271ee3f123f71bbf84998

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -60,7 +60,7 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len) {
 #else
 // 	uint32_t laddr = vaddr;
 	if(cpu.cr0.pe) {
-		laddr = segment_translate(vaddr, sreg);
+// 		laddr = segment_translate(vaddr, sreg);
 	}
 	return 0;
 // 	return laddr_read(laddr, len);
