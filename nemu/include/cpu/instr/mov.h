7cebfb4cebffb53d9e93d2522fae1622f6ae0c07
tracer-pa-public <tracer@njuics.org>
2021-12-05 11:22:55 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:22:55 up 101 days, 27 min,  4 users,  load average: 2.85, 2.44, 2.42 > unitime > 99d690c02d00edbeaf5821ea10870daf963417c9

--- nemu/include/cpu/instr/mov.h
+++ nemu/include/cpu/instr/mov.h
@@ -20,7 +20,7 @@ make_instr_func(mov_srm162r_l);
 
 make_instr_func(mov_rm2s_w);
 make_instr_func(mov_c2r_l);
-make_instr_fun(mov_r2c_l);
+make_instr_func(mov_r2c_l);
 
 make_instr_func(movs_v);
 make_instr_func(movs_b);
