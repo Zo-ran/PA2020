64e95f5ab55ea6ba512931914f0ad013ea8a010d
tracer-pa-public <tracer@njuics.org>
2021-12-20 23:17:53 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:17:53 up 116 days, 12:22,  5 users,  load average: 2.28, 2.50, 2.57 > unitime 1640013472989 > cf72184ee0e79bca86174ceccbf9e0c45d964f23

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -23,22 +23,22 @@ paddr_t page_translate(laddr_t laddr)
 // 	assert(pte.present == 1);
 // 	paddr_t res_addr = (pte.page_frame << 12) + offset;
 // 	return res_addr;
-uint32_t dir=(laddr>>22)&0x3ff;
-	uint32_t page=(laddr&0x3ff000)>>12;
-	uint32_t offset=laddr&0xfff;
-	PDE pde;
-	uint32_t addr_pde=(cpu.cr3.pdbr<<12)+4*dir;
-	memcpy(&pde.val,(void*)(hw_mem+addr_pde),4);
-	if(pde.present==0)
-	{
-	    printf("\n%x\n",cpu.eip);
-	}
-    assert(pde.present==0);
-	uint32_t addr_pte=(pde.page_frame<<12)+page*4;
-	PTE pte;
-	memcpy(&pte.val,(void*)(hw_mem+addr_pte),4);
-	assert(pte.present==1);
-	return (pte.page_frame<<12)+offset;
+// uint32_t dir=(laddr>>22)&0x3ff;
+// 	uint32_t page=(laddr&0x3ff000)>>12;
+// 	uint32_t offset=laddr&0xfff;
+// 	PDE pde;
+// 	uint32_t addr_pde=(cpu.cr3.pdbr<<12)+4*dir;
+// 	memcpy(&pde.val,(void*)(hw_mem+addr_pde),4);
+// 	if(pde.present==0)
+// 	{
+// 	    printf("\n%x\n",cpu.eip);
+// 	}
+//     assert(pde.present==0);
+// 	uint32_t addr_pte=(pde.page_frame<<12)+page*4;
+// 	PTE pte;
+// 	memcpy(&pte.val,(void*)(hw_mem+addr_pte),4);
+// 	assert(pte.present==1);
+// 	return (pte.page_frame<<12)+offset;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
