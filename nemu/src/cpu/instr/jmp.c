75a5705a621da5566d5a687e068628509054fdae
tracer-pa-public <tracer@njuics.org>
2021-12-21 17:11:14 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:11:14 up 117 days,  6:15,  5 users,  load average: 2.61, 2.63, 2.70 > unitime > 160c7f593d6982ff148446113da56f4b751682a4

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -50,61 +50,18 @@ make_instr_func(jmp_near_indirect)
 }
 
 make_instr_func(jmp_far_imm) {
-    // OPERAND imm;
-    // imm.sreg = SREG_CS;
-    // imm.type = OPR_IMM;
-    // imm.data_size=data_size;
-    // imm.addr = eip + 1;
-    // operand_read(&imm);
-    // imm.val = imm.val & (0xFFFFFFFF >> (32 - data_size));
-    // cpu.eip = imm.val;
-    // imm.addr += data_size / 8;
-    // imm.data_size = 16;
-    // operand_read(&imm);
-    // cpu.segReg[1].val = imm.val;
-    // load_sreg(1);
-    // return 0;
-        OPERAND val;
-    val.type=OPR_IMM;
-    val.data_size=data_size;
-    val.addr=eip+1;
-    val.sreg=SREG_CS;
-    operand_read(&val);
-    switch(data_size)
-    {
-        case 8:
-            val.val=val.val&0xff;
-            break;
-        case 16:
-            val.val=val.val&0xffff;
-            break;
-        case 32:
-            val.val=val.val&0xffffffff;
-            break;
-        default:
-            val.val=val.val;
-            break;
-    }
-    cpu.eip=val.val;
-    switch(data_size)
-    {
-        case 8:
-            val.addr=eip+2;
-            break;
-        case 16:
-            val.addr=eip+3;
-            break;
-        case 32:
-            val.addr=eip+5;
-            break;
-        default:
-            val.addr=val.addr;
-            break;
-    }
-    val.type=OPR_IMM;
-    val.data_size=16;
-    operand_read(&val);
-    cpu.segReg[1].val=val.val;
+    OPERAND imm;
+    imm.sreg = SREG_CS;
+    imm.type = OPR_IMM;
+    imm.data_size=data_size;
+    imm.addr = eip + 1;
+    operand_read(&imm);
+    imm.val = imm.val & (0xFFFFFFFF >> (32 - data_size));
+    cpu.eip = imm.val;
+    imm.addr += data_size / 8;
+    imm.data_size = 16;
+    operand_read(&imm);
+    cpu.segReg[1].val = imm.val;
     load_sreg(1);
     return 0;
 }
