e0cdf98e86e94bef16de93ec89ffee6b09321290
tracer-pa-public <tracer@njuics.org>
2021-12-21 11:19:27 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:19:27 up 117 days, 23 min,  5 users,  load average: 2.35, 2.68, 2.80 > unitime 1640056767128 > 567ebfa766c438b4e6f8752aadcd5738d385efb5

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -7,8 +7,8 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    //opr_dest.val = sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - data_size)), opr_dest.data_size);
-    //opr_src.val = sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - data_size)), opr_src.data_size);
+    opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
+    opr_dest.val = sign_ext(opr_dest.val, opr_dest.data_size);
     opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
     operand_write(&opr_dest);
 }
@@ -17,29 +17,8 @@ make_instr_impl_2op(and, i, rm, v)
 make_instr_impl_2op(and, i, rm, b)
 make_instr_impl_2op(and, i, a, b)
 make_instr_impl_2op(and, i, a, v)
-//make_instr_impl_2op(and, i, rm, bv)
+make_instr_impl_2op(and, i, rm, bv)
 make_instr_impl_2op(and, r, rm, b)
 make_instr_impl_2op(and, r, rm, v)
 make_instr_impl_2op(and, rm, r, b)
 make_instr_impl_2op(and, rm, r, v)
-
-make_instr_func(and_i2rm_bv)
-{
-    int len = 1;
-    OPERAND imm, rm;
-    
-    rm.data_size = data_size;
-    len += modrm_rm(eip + 1, &rm);
-    
-    imm.type = OPR_IMM;
-    imm.data_size = 8;
-    imm.addr = eip + len;
-    imm.sreg = SREG_CS;
-    operand_read(&imm);
-    operand_read(&rm);
-    imm.val = sign_ext(imm.val, 8);
-  
-    rm.val = alu_and(imm.val, rm.val, data_size);
-    operand_write(&rm);
-    return len + 1;
-}
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,81 +1,99 @@
 #include "cpu/instr.h"
 
-make_instr_func(push_r_v)
+static void instr_execute_1op()
 {
-    cpu.esp -= 4;
-    
-    OPERAND r, m;
-    r.type = OPR_REG;
-    r.data_size = data_size;
-    r.addr = opcode & 0x7;
-    m.type = OPR_MEM;
-    m.data_size = data_size;
-    m.addr = cpu.esp;
-    m.sreg = SREG_SS;
-    
-    operand_read(&r);
-    m.val = sign_ext(r.val & (0xFFFFFFFF >> (32 - data_size)), data_size);
-    operand_write(&m);
-    return 1;
+    OPERAND get;
+    operand_read(&opr_src);
+    cpu.esp=cpu.esp-data_size/8;
+    get.type=OPR_MEM;
+    get.addr=cpu.esp;
+    get.val=opr_src.val;
+    get.data_size=data_size;
+    get.sreg=SREG_SS;
+    operand_write(&get);
 }
 
-make_instr_func(push_i_b)
-{
-    cpu.esp -= 4;
-    
-    OPERAND imm, m;
-    imm.type = OPR_IMM;
-    imm.data_size = 8;
-    imm.addr = eip + 1;
-    m.type = OPR_MEM;
-    m.data_size = data_size;
-    m.addr = cpu.esp;
-    m.sreg = SREG_SS;
-    
-    operand_read(&imm);
-    m.val = sign_ext(imm.val & 0xFF, 8);
-    operand_write(&m);
-    return 2;
-}
+make_instr_impl_1op(push,i,b);
+make_instr_impl_1op(push,i,v);
+make_instr_impl_1op(push,r,v);
+make_instr_impl_1op(push,rm,v);
 
-make_instr_func(push_rm_v)
-{
-    int len = 1;
-    cpu.esp -= 4;
+// make_instr_func(push_r_v)
+// {
+//     cpu.esp -= 4;
     
-    OPERAND rm, m;
-    rm.data_size = data_size;
-    len += modrm_rm(eip + 1, &rm);
-    m.addr = cpu.esp;
-    m.type = OPR_MEM;
-    m.data_size = data_size;
-    m.sreg = SREG_SS;
-    operand_read(&rm);
-    m.val = sign_ext(rm.val, data_size);
-
-    operand_write(&m);
-    return len;
-}
-
-make_instr_func(push_i_v)
-{
-    // read imm
-    OPERAND imm;
-    imm.data_size = data_size;
-    imm.type = OPR_IMM;
-    imm.addr = eip + 1;
-    imm.sreg = SREG_CS;
-    operand_read(&imm);
+//     OPERAND r, m;
+//     r.type = OPR_REG;
+//     r.data_size = data_size;
+//     r.addr = opcode & 0x7;
+//     m.type = OPR_MEM;
+//     m.data_size = data_size;
+//     m.addr = cpu.esp;
+//     m.sreg = SREG_SS;
     
-    // push imm
-    cpu.esp -= 4;
-    OPERAND m;
-    m.data_size = data_size;
-    m.sreg = SREG_SS;
-    m.type = OPR_MEM;
-    m.addr = cpu.esp;
-    m.val = sign_ext(imm.val, data_size);
-    operand_write(&m);
+//     operand_read(&r);
+//     m.val = sign_ext(r.val & (0xFFFFFFFF >> (32 - data_size)), data_size);
+//     operand_write(&m);
+//     return 1;
+// }
+
+// make_instr_func(push_i_b)
+// {
+//     cpu.esp -= 4;
+    
+//     OPERAND imm, m;
+//     imm.type = OPR_IMM;
+//     imm.data_size = 8;
+//     imm.addr = eip + 1;
+//     m.type = OPR_MEM;
+//     m.data_size = data_size;
+//     m.addr = cpu.esp;
+//     m.sreg = SREG_SS;
+    
+//     operand_read(&imm);
+//     m.val = sign_ext(imm.val & 0xFF, 8);
+//     operand_write(&m);
+//     return 2;
+// }
+
+// make_instr_func(push_rm_v)
+// {
+//     int len = 1;
+//     cpu.esp -= 4;
+    
+//     OPERAND rm, m;
+//     rm.data_size = data_size;
+//     len += modrm_rm(eip + 1, &rm);
+//     m.addr = cpu.esp;
+//     m.type = OPR_MEM;
+//     m.data_size = data_size;
+//     m.sreg = SREG_SS;
+//     operand_read(&rm);
+//     m.val = sign_ext(rm.val, data_size);
+
+//     operand_write(&m);
+//     return len;
+// }
+
+// make_instr_func(push_i_v)
+// {
+//     // read imm
+//     OPERAND imm;
+//     imm.data_size = data_size;
+//     imm.type = OPR_IMM;
+//     imm.addr = eip + 1;
+//     imm.sreg = SREG_CS;
+//     operand_read(&imm);
+    
+//     // push imm
+//     cpu.esp -= 4;
+//     OPERAND m;
+//     m.data_size = data_size;
+//     m.sreg = SREG_SS;
+//     m.type = OPR_MEM;
+//     m.addr = cpu.esp;
+//     m.val = sign_ext(imm.val, data_size);
+//     operand_write(&m);
      
-    return 1 + data_size / 8;
-}
+//     return 1 + data_size / 8;
+// }
