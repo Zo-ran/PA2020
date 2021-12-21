d08358c6047079009069a4c2538f1b96a6f9d3a8
tracer-pa-public <tracer@njuics.org>
2021-12-21 17:06:50 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:06:50 up 117 days,  6:11,  5 users,  load average: 2.58, 2.72, 2.74 > unitime > 2fbbdd12234ec7b48530f0e06ae9bb3c731040bd

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,17 +3,33 @@
 Put the implementations of `lgdt' instructions here.
 */
 make_instr_func(lgdt) {
-    int len = 1;
-    OPERAND MEM;        
-    MEM.data_size = data_size;
-    len += modrm_rm(eip + 1, &MEM);
-    MEM.data_size = 16;
-    operand_read(&MEM);
-    cpu.gdtr.limit = MEM.val & 0xffff;
-    MEM.data_size = 32;
-    MEM.addr += 2;
-    operand_read(&MEM);
-    cpu.gdtr.base = MEM.val & 0xffffffff;
-    
+        int len=1;
+    opr_src.data_size=data_size;
+    len=len+modrm_rm(eip+1,&opr_src);
+    OPERAND a;
+    a.data_size=16;
+    a.type=OPR_MEM;
+    a.sreg=opr_src.sreg;
+    a.addr=opr_src.addr;
+    operand_read(&a);
+    cpu.gdtr.limit=a.val&0xffff;
+    a.type=OPR_MEM;
+    a.data_size=32;
+    a.addr+=2;
+    operand_read(&a);
+    cpu.gdtr.base=a.val&0xffffffff;
     return len;
+    // int len = 1;
+    // OPERAND MEM;        
+    // MEM.data_size = data_size;
+    // len += modrm_rm(eip + 1, &MEM);
+    // MEM.data_size = 16;
+    // operand_read(&MEM);
+    // cpu.gdtr.limit = MEM.val & 0xffff;
+    // MEM.data_size = 32;
+    // MEM.addr += 2;
+    // operand_read(&MEM);
+    // cpu.gdtr.base = MEM.val & 0xffffffff;
+    
+    // return len;
 }
