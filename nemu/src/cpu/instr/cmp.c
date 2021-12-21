76058389c6a42acf7ea7723044054b96ab68cc7f
tracer-pa-public <tracer@njuics.org>
2021-12-21 10:39:20 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:39:20 up 116 days, 23:43,  5 users,  load average: 2.41, 2.89, 2.92 > unitime > 589abd54887cba58e77ea6885b2e5c6ec964fa0a

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -4,8 +4,8 @@
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_src.val = sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - data_size)), opr_src.data_size);
-    opr_dest.val = sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - data_size)), opr_dest.data_size);
+    opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
+    opr_dest.val = sign_ext(opr_dest.val, opr_dest.data_size);
     alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);    
 }
 
@@ -16,35 +16,5 @@ make_instr_impl_2op(cmp, r, rm, b)
 make_instr_impl_2op(cmp, r, rm, v)
 make_instr_impl_2op(cmp, rm, r, b)
 make_instr_impl_2op(cmp, rm, r, v)
-
-make_instr_func(cmp_i2a_b)
-{
-    OPERAND imm;
-    imm.type = OPR_IMM;
-    imm.addr = eip + 1;
-    imm.data_size = 8;
-    imm.sreg = SREG_CS;
-    operand_read(&imm);
-    
-    alu_sub(imm.val, cpu.eax, 8);
-    return 2;
-}
-
-make_instr_func(cmp_i2rm_bv)
-{
-    int len = 1;
-    OPERAND imm, rm;
-    
-    rm.data_size = data_size;
-    len += modrm_rm(eip + 1, &rm);
-    imm.sreg = SREG_CS;
-    imm.type = OPR_IMM;
-    imm.data_size = 8;
-    imm.addr = eip + len;
-    operand_read(&imm);
-    operand_read(&rm);
-    imm.val = sign_ext(imm.val & 0xFF, 8);
-  
-    alu_sub(imm.val, rm.val, data_size);
-    return len + 1;
-}
+make_instr_impl_2op(cmp, i, a, b)
+make_instr_impl_2op(cmp, i, rm, bv)
