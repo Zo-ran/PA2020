ecc0806f8dbf8c60e72b00b571f1f5f2d9e8dc4f
tracer-pa-public <tracer@njuics.org>
2021-12-22 12:17:13 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:17:13 up 118 days,  1:21,  5 users,  load average: 2.55, 2.53, 2.54 > unitime 1640146633380 > 10e091aa6980c0bee1b377b2b2a9f4afa3c070d0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,8 +49,8 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 	return paddr_read(laddr, len);
 #else
     paddr_t paddr = laddr;
-    printf("\e[0;31mpe: %d, pg: %d\e[0m\n", cpu.cr0.pe, cpu.cr0.pg);
-    fflush(stdout);   
+    // printf("\e[0;31mpe: %d, pg: %d\e[0m\n", cpu.cr0.pe, cpu.cr0.pg);
+    // fflush(stdout);   
     if(cpu.cr0.pe && cpu.cr0.pg) {
         if((laddr & 0x00000fff) + len > 0x1000) {   //处理跨页的情况
             uint32_t next_len = (laddr & 0x00000fff) + len - 0x1000;
