264cc1bda2124749311ec7dd1b9487d4ee62ca01
tracer-pa-public <tracer@njuics.org>
2021-12-22 12:36:31 +0800

    > compile nemu testcase kernel > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:36:31 up 118 days,  1:40,  5 users,  load average: 2.47, 2.50, 2.53 > unitime 1640147791454 > d5c25d98bd1f43e5473daa8bfb2b2c372a8be9ad

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -4,8 +4,6 @@
 // return the linear address from the virtual address and segment selector
 uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 {
-	printf("\e[0;31mbase: %x\e[0m\n", cpu.segReg[sreg].base);
-    fflush(stdout);   
     return cpu.segReg[sreg].base + offset;
 }
 
@@ -16,7 +14,6 @@ void load_sreg(uint8_t sreg)
 	SegDesc *segdesc = (SegDesc *)hw_mem + offset;
 	segdesc->val[0] = laddr_read(offset, 4);
 	segdesc->val[1] = laddr_read(offset + 4, 4);
-	cpu.segReg[sreg].base = (segdesc->base_15_0 & 0xffff) + ((segdesc->base_23_16 & 0xff) << 16) + ((segdesc->base_31_24 & 0xff) << 24);
 	cpu.segReg[sreg].base = 0;
 	cpu.segReg[sreg].limit = (segdesc->limit_15_0 & 0xffff) + ((segdesc->limit_19_16 & 0xf) << 16);
 	cpu.segReg[sreg].type = (segdesc->type & 0xf) + ((segdesc->segment_type & 0x1) << 4);
