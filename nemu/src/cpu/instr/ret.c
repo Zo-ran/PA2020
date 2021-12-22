d21e2510cffd168624d2e2d6378ab132dcf8fc6d
tracer-pa-public <tracer@njuics.org>
2021-12-22 13:58:49 +0800

    > clean > id 201870049 > user pa201870049 > uname Linux 378ab9c8258c 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 13:58:49 up 118 days,  3:03,  5 users,  load average: 2.60, 2.60, 2.64 > unitime > c0faa6f8fda45740c3566bf69aaa7126ff01340e

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -4,31 +4,14 @@ Put the implementations of `ret' instructions here.
 */
 make_instr_func(ret_near)
 {
-    // OPERAND MEM;
-    // MEM.data_size = data_size;
-    // MEM.type = OPR_MEM;
-    // MEM.addr = cpu.esp;
-    // MEM.sreg = SREG_SS;
-    // operand_read(&MEM);
-    // cpu.eip = sign_ext(MEM.val, data_size);
-    // cpu.esp += 4;
-        OPERAND top;
-    top.type=OPR_MEM;
-    top.data_size=data_size;
-    top.addr=cpu.esp;
-    top.sreg=SREG_SS;
-    operand_read(&top);
-    if(data_size==32)
-    {
-        cpu.eip=top.val;
-        cpu.esp+=0x4;
-    }
-    if(data_size==16)
-    {
-        cpu.esp+=0x2;
-        cpu.eip=top.val&0xffff;
-    }
-    return 0;
+    OPERAND MEM;
+    MEM.data_size = data_size;
+    MEM.type = OPR_MEM;
+    MEM.addr = cpu.esp;
+    MEM.sreg = SREG_SS;
+    operand_read(&MEM);
+    cpu.eip = sign_ext(MEM.val, data_size);
+    cpu.esp += 4;
 
     return 0;
 }
